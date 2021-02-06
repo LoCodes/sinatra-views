ad883f24f820390197287d53040efdf8a7367435
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-06 10:08:08 -0700

    commit tree 39f38d30524600806b341f51cee1f34eb8767bf8

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
+	get '/' do				#run index.erb
+		erb :index
 	end
+
+	get "/info" do
+		erb :info
+   	 end 
+
 end
